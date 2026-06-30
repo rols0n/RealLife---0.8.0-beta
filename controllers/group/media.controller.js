861dbf418edf8e755db83fc9779b22155c7f3370
Patryk Rolka <rolson.priv@gmail.com>
2026-06-30 06:12:45 +0200

    refactor(group): implemented asyncHandler and reduced code duplications

--- controllers/group/media.controller.js
+++ controllers/group/media.controller.js
@@ -1,22 +1,17 @@
 const Group = require("../../models/groupModel");
-const User = require("../../models/userModel");
-
-
-
-const asyncHandler = require("../../middlewares/utils/asyncHandler")
-const AppError = require("../../middlewares/utils/AppError")
-
-
+const  asyncHandler  = require("../../middlewares/utils/asyncHandler");
+const AppError = require("../../middlewares/utils/AppError");
 
 const multer = require("multer");
 const sharp = require("sharp");
+
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, callBack) => {
   if (file.mimetype.startsWith("image")) {
     callBack(null, true);
   } else {
-    callBack("You can upload ONLY images", false);
+    callBack(new AppError("You can upload ONLY images", 400, "ONLY_IMAGES_ALLOWED"), false);
   }
 };
 
@@ -25,54 +20,62 @@ const upload = multer({
   fileFilter: multerFilter,
 });
 
+const resizeGroupImage = (type, size) =>
+  asyncHandler(async (req, res, next) => {
+    if (!req.file) return next();
 
-const resizeImage = async (type, size, req,res,next) => {
-  if(!req.file) next(new AppError("File not found", 404, "NOT_FOUND"));
-  const group = await Group.findById(req.params.id);
-  req.file.filename = `${type}-image-${group._id}.jpeg`;
-  await sharp(req.file.buffer)
-    .resize(size[0], size[1])
-    .toFormat("jpeg")
-    .jpeg({quality: 95})
-    .toFile(`public/imgs/groups/${group._id}/${type}/${req.file.filename}`)
-  next();
-  }
-
-const uploadImage = async (type, field, req,res,next) => {
- 
-    
     const group = await Group.findById(req.params.id);
-    if (group) {
-      if (!req.file) {
-        next(new AppError("This route is only for changing group images", 400, "BAD_REQUEST"))
-        
-      }
 
-      const imagePath = `/imgs/groups/${group._id}/${type}/${req.file.filename}`;
-      await Group.findByIdAndUpdate(group._id, {
-        field: imagePath,
-      });
-
-      res.status(200).json({
-        status: "success",
-        data: { imagePath },
-      });
+    if (!group) {
+      return next(new AppError("Group not found", 404, "GROUP_NOT_FOUND"));
     }
-  
-}
 
+    req.group = group;
+    req.file.filename = `${type}-image-${group._id}.jpeg`;
+
+    await sharp(req.file.buffer)
+      .resize(size[0], size[1])
+      .toFormat("jpeg")
+      .jpeg({ quality: 95 })
+      .toFile(`public/imgs/groups/${group._id}/${type}/${req.file.filename}`);
+
+    next();
+  });
+
+const updateGroupImage = (type, field) =>
+  asyncHandler(async (req, res, next) => {
+    const group = req.group || await Group.findById(req.params.id);
+
+    if (!group) {
+      return next(new AppError("Group not found", 404, "GROUP_NOT_FOUND"));
+    }
+
+    if (!req.file) {
+      return next(
+        new AppError(
+          "This route is only for changing group images",
+          400,
+          "GROUP_IMAGES_ONLY"
+        )
+      );
+    }
+
+    const imagePath = `/imgs/groups/${group._id}/${type}/${req.file.filename}`;
+
+    await Group.findByIdAndUpdate(group._id, {
+      [field]: imagePath,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: { imagePath },
+    });
+  });
 
 exports.uploadAvatarImage = upload.single("avatarImage");
-exports.resizeAvatarImage = async (req, res, next) => resizeImage("avatars", [500, 500], req,res,next);
+exports.resizeAvatarImage = resizeGroupImage("avatars", [500, 500]);
+exports.updateAvatarImage = updateGroupImage("avatars", "avatarImage");
 
-exports.updateAvatarImage = asyncHandler(async (req, res, next) => {
-  await uploadImage("avatars", avatarImage, req,res,next)
-});
-
-// Banner
 exports.uploadBannerImage = upload.single("bannerImage");
-exports.resizeBannerImage = async (req, res, next) => resizeImage("banners", [1250, 500], req,res,next);;
-
-exports.updateBannerImage = asyncHandler(async (req, res, next) => {
-  await uploadImage("banners", bannerImage, req,res,next)
-});
+exports.resizeBannerImage = resizeGroupImage("banners", [1250, 500]);
+exports.updateBannerImage = updateGroupImage("banners", "bannerImage");
