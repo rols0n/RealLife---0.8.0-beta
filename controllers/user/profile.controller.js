d2dae6fe8e8866681855bd87f81e1d164a476caa
Patryk Rolka <rolson.priv@gmail.com>
2026-07-01 06:23:04 +0200

    refactor user image controller

--- controllers/user/profile.controller.js
+++ controllers/user/profile.controller.js
@@ -1,129 +1,103 @@
 const User = require("../../models/userModel");
-const handlerController = require("../../controllers/handlerController");
+
 const multer = require("multer");
 const sharp = require("sharp");
-const fs = require("fs");
-const jwt = require("jsonwebtoken");
 
 const decodingToken = require("../../utils/decodingToken");
+const asyncHandler = require("../../middlewares/utils/asyncHandler");
+
+const AppError = require("../../middlewares/utils/AppError");
 
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, callBack) => {
   if (file.mimetype.startsWith("image")) {
-    callBack(null, true);
-  } else {
-    callBack("You can upload ONLY images", false);
+    return callBack(null, true);
   }
-};
 
+  callBack(new AppError("You can upload ONLY images", 400, "WRONG_FILE_TYPE"), false);
+};
 
 const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
 });
 
+const getLoggedUser = async (req) => {
+  if (req.user) return req.user;
+
+  const decoded = await decodingToken(req);
+  const user = await User.findById(decoded.id);
+
+  if (!user) {
+    throw new AppError("User doesn't exist", 404, "USER_NOT_FOUND");
+  }
+
+  req.user = user;
+  return user;
+};
+
+const resizeUserImage = (folder, fileName, width, height) =>
+  asyncHandler(async (req, res, next) => {
+    if (!req.file) return next();
+
+    const user = await getLoggedUser(req);
+
+    req.file.filename = `${fileName}-${user._id}.jpeg`;
+
+    await sharp(req.file.buffer)
+      .resize(width, height)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(`public/imgs/users/${user._id}/${folder}/${req.file.filename}`);
+
+    next();
+  });
+
+const updateUserImage = (field, folder) =>
+  asyncHandler(async (req, res, next) => {
+    const user = await getLoggedUser(req);
+
+    if (!req.file) {
+      return next(
+        new AppError(
+          "This route is only for changing user images",
+          400,
+          "IMAGE_NOT_FOUND"
+        )
+      );
+    }
+
+    const imagePath = `/imgs/users/${user._id}/${folder}/${req.file.filename}`;
+
+    await User.findByIdAndUpdate(user._id, {
+      [field]: imagePath,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: { imagePath },
+    });
+  });
 
 exports.uploadProfilePicture = upload.single("profileImage");
-exports.resizeProfilePicture = async (req, res, next) => {
-  if (!req.file) return next();
 
-  const decoded = await decodingToken(req);
+exports.resizeProfilePicture = resizeUserImage(
+  "profilePicture",
+  "profile-picture",
+  500,
+  500
+);
 
-  // 3) Checking if USER still exists
-  const user = await User.findById(decoded.id);
-
-  req.file.filename = `profile-picture-${user._id}.jpeg`;
-  await sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat("jpeg")
-    .jpeg({ quality: 90 })
-    .toFile(`public/imgs/users/${user._id}/profilePicture/${req.file.filename}`);
-
-  next();
-};
-exports.updateProfilePicture = async (req, res, next) => {
-  try {
-    // const user = await User.findById(req.params.id);
-    const decoded = await decodingToken(req);
-
-    // 3) Checking if USER still exists
-    const user = await User.findById(decoded.id);
-    if (user) {
-      if (!req.file) {
-        res.status(404).json({
-          status: "fail",
-          message: "This route is only for changing user images",
-        });
-      }
-      console.log(req.file);
-
-      const imagePath = `/imgs/users/${user._id}/profilePicture/${req.file.filename}`;
-      await User.findByIdAndUpdate(user._id, {
-        profileImage: imagePath,
-      });
-
-      res.status(200).json({
-        status: "success",
-        data: { imagePath },
-      });
-    }
-  } catch (err) {
-    res.status(404).json({ status: "fail", error: err });
-  }
-};
-
-
+exports.updateProfilePicture = updateUserImage("profileImage", "profilePicture");
 
 exports.uploadBannerPicture = upload.single("bannerPicture");
-exports.resizeBannerPicture = async (req, res, next) => {
-  if (!req.file) return next();
-  const decoded = await decodingToken(req);
 
-  // 3) Checking if USER still exists
-  const user = await User.findById(decoded.id);
+exports.resizeBannerPicture = resizeUserImage(
+  "bannerPicture",
+  "banner-picture",
+  1200,
+  628
+);
 
-  req.file.filename = `banner-picture-${user._id}.jpeg`;
-  await sharp(req.file.buffer)
-    .resize(1200, 628)
-    .toFormat("jpeg")
-    .jpeg({ quality: 90 })
-    .toFile(`public/imgs/users/${user._id}/bannerPicture/${req.file.filename}`);
-
-  next();
-};
-exports.updateBannerPicture = async (req, res, next) => {
-  try {
-    const decoded = await decodingToken(req);
-
-    // 3) Checking if USER still exists
-    const user = await User.findById(decoded.id);
-
-    if (user) {
-      if (!req.file) {
-        res.status(404).json({
-          status: "fail",
-          message: "This route is only for changing user images",
-        });
-      }
-
-      // console.log(req.file);
-      const imagePath = `/imgs/users/${user._id}/bannerPicture/${req.file.filename}`;
-      await User.findByIdAndUpdate(user._id, {
-        bannerImage: imagePath,
-      });
-
-      res.status(200).json({
-        status: "success",
-        data: { imagePath },
-      });
-    }
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err,
-    });
-  }
-};
-
-
+exports.updateBannerPicture = updateUserImage("bannerImage", "bannerPicture");
