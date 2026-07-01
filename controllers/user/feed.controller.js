23557149561158cb3479c88a5cb670eb4d3c889a
Patryk Rolka <rolson.priv@gmail.com>
2026-07-01 06:49:26 +0200

    refactor: clean up feed content generation

--- controllers/user/feed.controller.js
+++ controllers/user/feed.controller.js
@@ -1,31 +1,45 @@
 const User = require("../../models/userModel");
 const decodingToken = require("../../utils/decodingToken");
+const AppError = require("../../middlewares/utils/AppError");
 
-const {asyncHandler} = require("../../middlewares/utils/asyncHandler")
+const { asyncHandler } = require("../../middlewares/utils/asyncHandler");
 
+// ---------------------------
+// Populate configs
+// ---------------------------
 
+const POST_SELECT =
+  "createdAt author images cantBeDisplayedBy reactions postText comments postImages";
 
+const USER_PREVIEW_SELECT =
+  "firstName lastName profileImage bannerImage tree";
+
+const COMMENT_TREE_SELECT =
+  "firstName lastName profileImage bannerImage author repliesTo text createdAt reactions subReplies";
+
+const SUB_REPLY_SELECT =
+  "firstName lastName profileImage bannerImage author repliesTo text createdAt reactions repliesToComment";
+
+const AUTHOR_PREVIEW_SELECT =
+  "firstName lastName profileImage bannerImage";
 
 const postPopulate = {
   path: "_id",
-  select:
-    "createdAt author images cantBeDisplayedBy reactions postText comments postImages",
+  select: POST_SELECT,
   populate: {
     path: "author comments",
-    select: "firstName lastName profileImage bannerImage tree",
+    select: USER_PREVIEW_SELECT,
     populate: {
       path: "tree",
       populate: {
         path: "author replies",
-        select:
-          "firstName lastName profileImage bannerImage author repliesTo text createdAt reactions subReplies",
+        select: COMMENT_TREE_SELECT,
         populate: {
           path: "author subReplies",
-          select:
-            "firstName lastName profileImage bannerImage author repliesTo text createdAt reactions repliesToComment",
+          select: SUB_REPLY_SELECT,
           populate: {
             path: "author",
-            select: "firstName lastName profileImage bannerImage",
+            select: AUTHOR_PREVIEW_SELECT,
           },
         },
       },
@@ -47,52 +61,85 @@ const friendsPostsPopulate = {
   },
 };
 
+// ---------------------------
+// Helpers
+// ---------------------------
+
+const toStringId = (id) => id?.toString();
+
+const isSameId = (firstId, secondId) => {
+  return toStringId(firstId) === toStringId(secondId);
+};
+
+const isUserBlockedFromPost = (post, userId) => {
+  const blockedUsers = post?.cantBeDisplayedBy || [];
+
+  return blockedUsers.some((blockedUserId) =>
+    isSameId(blockedUserId, userId)
+  );
+};
+
+const isUserPagePost = (postWrapper) => {
+  return postWrapper?.place === "userPage";
+};
+
+const hasPopulatedPost = (postWrapper) => {
+  return Boolean(postWrapper?._id);
+};
+
+const canDisplayPost = (postWrapper, userId) => {
+  if (!postWrapper) return false;
+  if (!isUserPagePost(postWrapper)) return false;
+  if (!hasPopulatedPost(postWrapper)) return false;
+
+  return !isUserBlockedFromPost(postWrapper._id, userId);
+};
+
+const collectVisiblePosts = (postWrappers = [], userId) => {
+  return postWrappers
+    .filter((postWrapper) => canDisplayPost(postWrapper, userId))
+    .map((postWrapper) => postWrapper._id);
+};
+
+const collectFriendsVisiblePosts = (friends = [], userId) => {
+  return friends.flatMap((friend) =>
+    collectVisiblePosts(friend.posts, userId)
+  );
+};
+
+const sortPostsByNewest = (posts) => {
+  return posts.sort(
+    (firstPost, secondPost) =>
+      new Date(secondPost.createdAt) - new Date(firstPost.createdAt)
+  );
+};
+
+// ---------------------------
+// Controller
+// ---------------------------
+
 exports.generateFeedContent = asyncHandler(async (req, res, next) => {
-  
-    const isUserBannedFromPost = (post, userId) => {
-      if (!post?.cantBeDisplayedBy) return false;
+  const decoded = await decodingToken(req);
+  const userId = decoded.id;
 
-      return post.cantBeDisplayedBy.some(
-        bannedUserId => bannedUserId.toString() === userId.toString()
-      );
-    };
+  const user = await User.findById(userId)
+    .select("posts friends")
+    .populate(userPostsPopulate)
+    .populate(friendsPostsPopulate)
+    .lean();
 
-    const canDisplayPost = (postWrapper, userId) => {
-      if (!postWrapper) return false;
-      if (postWrapper.place !== "userPage") return false;
-      if (!postWrapper._id) return false;
+  if (!user) {
+    return next(new AppError("User not found", 404, "USER_NOT_FOUND"));
+  }
 
-      return !isUserBannedFromPost(postWrapper._id, userId);
-    };
+  const userPosts = collectVisiblePosts(user.posts, userId);
+  const friendsPosts = collectFriendsVisiblePosts(user.friends, userId);
 
-    const collectVisiblePosts = (postWrappers, userId) => {
-      return postWrappers
-        .filter(postWrapper => canDisplayPost(postWrapper, userId))
-        .map(postWrapper => postWrapper._id);
-    };
+  const feedPosts = sortPostsByNewest([...userPosts, ...friendsPosts]);
 
-
-
-    const decoded = await decodingToken(req);
-
-    const user = await User.findById(decoded.id)
-      .select("posts _id friends")
-      .populate(userPostsPopulate)
-      .populate(friendsPostsPopulate);
-
-    const posts = [];
-
-    posts.push(...collectVisiblePosts(user.posts, user._id));
-
-    for (const friend of user.friends) {
-      posts.push(...collectVisiblePosts(friend.posts, user._id));
-    }
-
-    posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
-    res.status(200).json({
-      status: "success",
-      data: posts,
-    });
-  
+  res.status(200).json({
+    status: "success",
+    results: feedPosts.length,
+    data: feedPosts,
+  });
 });
