5525e789f59dc003ca01f90ac3549be449a3ff27
Patryk Rolka <rolson.priv@gmail.com>
2026-07-01 07:09:42 +0200

    refactor: simplify comment reaction updates

--- controllers/utils/commentReaction.js
+++ controllers/utils/commentReaction.js
@@ -1,6 +1,87 @@
 const Comment = require("../../models/commentModel");
+const AppError = require("../../middlewares/utils/AppError");
 
-const generateReactionsObj = require("../../utils/generateReactionsObj.js");
+const generateReactionsObj = require("../../utils/generateReactionsObj");
+
+const getId = (value) => {
+  return value?._id || value;
+};
+
+const isSameId = (firstId, secondId) => {
+  return getId(firstId)?.toString() === getId(secondId)?.toString();
+};
+
+const updateReactions = (reactions, userID, reactionType, behaviour) => {
+  const { likes, disLikes, count } = generateReactionsObj(
+    behaviour,
+    reactions.disLikes,
+    reactions.likes,
+    userID,
+    reactionType,
+    reactions.order
+  );
+
+  return {
+    ...reactions,
+    likes,
+    disLikes,
+    count,
+  };
+};
+
+const matchesComment = (comment, commentID, author) => {
+  if (!isSameId(comment._id, commentID)) return false;
+  if (!author) return true;
+
+  return isSameId(comment.author, author);
+};
+
+const getReactionTarget = (comment, level, commentID, author) => {
+  if (level === "root") {
+    return {
+      target: comment.tree,
+      updatePath: "tree.reactions",
+    };
+  }
+
+  const replies = comment.tree.replies || [];
+
+  if (level === "reply") {
+    const replyIndex = replies.findIndex((reply) =>
+      matchesComment(reply, commentID, author)
+    );
+
+    if (replyIndex === -1) {
+      throw new AppError("Reply doesn't exist", 404, "REPLY_NOT_FOUND");
+    }
+
+    return {
+      target: replies[replyIndex],
+      updatePath: `tree.replies.${replyIndex}.reactions`,
+    };
+  }
+
+  if (level === "subReply") {
+    for (let replyIndex = 0; replyIndex < replies.length; replyIndex++) {
+      const subReplies = replies[replyIndex].subReplies || [];
+
+      const subReplyIndex = subReplies.findIndex((subReply) =>
+        matchesComment(subReply, commentID, author)
+      );
+
+      if (subReplyIndex !== -1) {
+        return {
+          target: subReplies[subReplyIndex],
+          updatePath: `tree.replies.${replyIndex}.subReplies.${subReplyIndex}.reactions`,
+        };
+      }
+    }
+
+    throw new AppError("Sub reply doesn't exist", 404, "SUB_REPLY_NOT_FOUND");
+  }
+
+  throw new AppError("Invalid comment level", 400, "INVALID_COMMENT_LEVEL");
+};
 
 module.exports = async (
   req,
@@ -13,149 +94,32 @@ module.exports = async (
   reactionType,
   behaviour
 ) => {
-  let comment;
+  const comment = await Comment.findById(treeID);
 
-  if (level === "root") {
-    comment = await Comment.findById(treeID);
-    let objHolder = comment.tree;
-
-    let {
-      likes: rawlikes,
-      disLikes: rawDisLikes,
-      order: rawOrder,
-    } = objHolder.reactions;
-
-    const { disLikes, likes, count } = generateReactionsObj(
-      behaviour,
-      rawDisLikes,
-      rawlikes,
-      userID,
-      reactionType,
-      rawOrder
-    );
-
-    objHolder.reactions.likes = likes;
-    objHolder.reactions.disLikes = disLikes;
-    objHolder.reactions.count = count;
-
-    await Comment.updateOne(
-      { _id: treeID },
-      {
-        $set: {
-          [`tree.reactions`]: objHolder.reactions,
-        },
-      }
-    );
+  if (!comment) {
+    throw new AppError("Comment tree doesn't exist", 404, "COMMENT_NOT_FOUND");
   }
 
-  if (level === "reply") {
-    strLevel = [`tree`, `replies`];
-    strLevel_ = "tree.replies";
-  } else if (level === "subReply") {
-    strLevel = [`tree`, `replies`, `subReplies`];
-    strLevel_ = "tree.replies.subReplies";
-  } else return;
-  // ------
-  // Getting commentObj that we will work on for subReplies/replies
-  if (level === "subReply" || level === "reply") {
-    // # strLevel_ is a path to the object in collection
-    comment = await Comment.findOne({
-      [`${strLevel_}.author`]: author,
-      [`${strLevel_}._id`]: commentID,
-      _id: treeID,
-    });
-  }
-  // ---
+  const { target, updatePath } = getReactionTarget(
+    comment,
+    level,
+    commentID,
+    author
+  );
 
-  if (level === "reply") {
-    let replyObj, indexOfReply;
+  const updatedReactions = updateReactions(
+    target.reactions,
+    userID,
+    reactionType,
+    behaviour
+  );
 
-    const replies = Array.from(comment.tree.replies);
-
-    replies.forEach((reply) => {
-      if (`${reply._id}` === `${commentID}`) {
-        indexOfReply = replies.indexOf(reply);
-        replyObj = reply;
-      }
-    });
-
-    let {
-      likes: rawlikes,
-      disLikes: rawDisLikes,
-      order: rawOrder,
-    } = replyObj.reactions;
-
-    const { disLikes, likes, count } = generateReactionsObj(
-      behaviour,
-      rawDisLikes,
-      rawlikes,
-      userID,
-      reactionType,
-      rawOrder
-    );
-
-    replyObj.reactions.likes = likes;
-    replyObj.reactions.disLikes = disLikes;
-    replyObj.reactions.count = count;
-
-    await Comment.updateOne(
-      { _id: treeID },
-      {
-        $set: {
-          [`tree.replies.${indexOfReply}.reactions`]: replyObj.reactions,
-        },
-      }
-    );
-  }
-  // ========
-  // Managing subReply
-  if (level === "subReply") {
-    let subReplyObj, replyIndex, subReplyIndex;
-
-    const replies = Array.from(comment.tree.replies);
-    replies.forEach((reply) => {
-      // Looping over replies, so we could loop over subReplies
-      const subReplies = Array.from(reply.subReplies);
-      subReplies.forEach((subReply) => {
-        // Looping over subReplies to find subReply that interests us
-        if (`${subReply._id}` === `${commentID}`) {
-          subReplyObj = subReply;
-
-          // Signing the index of reply and subReply so we could use them later for updating database
-          replyIndex = replies.indexOf(reply);
-          subReplyIndex = subReplies.indexOf(subReply);
-        }
-      });
-    });
-
-    let {
-      likes: rawlikes,
-      disLikes: rawDisLikes,
-      order: rawOrder,
-    } = subReplyObj.reactions;
-
-    const { disLikes, likes, count } = generateReactionsObj(
-      behaviour,
-      rawDisLikes,
-      rawlikes,
-      userID,
-      reactionType,
-      rawOrder
-    );
-
-    subReplyObj.reactions.likes = likes;
-    subReplyObj.reactions.disLikes = disLikes;
-    subReplyObj.reactions.count = count;
-
-    await Comment.updateOne(
-      { _id: treeID },
-      {
-        $set: {
-          [`tree.replies.${replyIndex}.subReplies.${subReplyIndex}`]:
-            subReplyObj,
-        },
-      }
-    );
-  }
-  // ===
+  await Comment.updateOne(
+    { _id: treeID },
+    {
+      $set: {
+        [updatePath]: updatedReactions,
+      },
+    }
+  );
 };
