89bf3bdd3bb194c1dc707fbe49b4e78b952b8d06
Patryk Rolka <rolson.priv@gmail.com>
2026-07-07 20:39:35 +0200

    refactor: simplify comment controller and fix reactions

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,205 +1,239 @@
 const Comment = require("../models/commentModel");
 const User = require("../models/userModel");
-const handlerController = require("../controllers/handlerController");
+
+const handlerController = require("./handlerController");
 
 const decodingToken = require("../utils/decodingToken");
-const commentReaction = require("./utils/commentReaction.js");
+const commentReaction = require("./utils/commentReaction");
 
-exports.getAllComments = async (req, res) => {
-  handlerController.getAll(req, res, Comment);
-};
+const {
+  asyncHandler,
+} = require("../middlewares/utils/asyncHandler");
+const AppError = require("../middlewares/utils/AppError");
 
-exports.createComment = async (req, res) => {
-  try {
-    const decoded = await decodingToken(req);
 
-    // 3) Checking if USER still exists
-    const user = await User.findById(decoded.id);
+// ====================
+// Helpers
+// ====================
 
-    req.body.tree.author = user._id;
-
-    const comment = await Comment.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: {
-        data: comment,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err,
-    });
-  }
-};
-
-exports.updateComment = async (req, res) => {
-  handlerController.update(req, res, Comment);
-};
-
-exports.getCommentById = async (req, res) => {
-  try {
-    const comment = await Comment.findById(req.params.id);
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        data: comment,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err: {
-        err,
-      },
-    });
-  }
-};
-
-exports.deleteComment = async (req, res) => {
-  handlerController.delete(req, res, Comment);
-};
-
-exports.updateLevelTwoRespond = async (req, res) => {
+const getLoggedUser = async (req) => {
   const decoded = await decodingToken(req);
 
-  // 3) Checking if USER still exists
   const user = await User.findById(decoded.id);
 
-  req.body.author = user._id;
-  await Comment.findByIdAndUpdate(req.params.id, {
-    $push: {
-      "tree.replies": req.body,
-    },
+  if (!user) {
+    throw new AppError(
+      "Logged user doesn't exist",
+      404,
+      "USER_NOT_FOUND"
+    );
+  }
+
+  return user;
+};
+
+const validateReactionData = ({ level, author, reactionType }) => {
+  if (!level || !author || !reactionType) {
+    throw new AppError(
+      'Body misses required data: "level", "author", "reactionType"',
+      400,
+      "MISSING_REACTION_DATA"
+    );
+  }
+
+  if (!["like", "disLike"].includes(reactionType)) {
+    throw new AppError(
+      '"reactionType" must be either "like" or "disLike"',
+      400,
+      "INVALID_REACTION_TYPE"
+    );
+  }
+};
+
+const handleCommentReaction = (action) =>
+  asyncHandler(async (req, res) => {
+    const treeID = req.params.treeID;
+    const decoded = await decodingToken(req);
+
+    const { level, author, reactionType, commentID } = req.body;
+
+    validateReactionData({
+      level,
+      author,
+      reactionType,
+    });
+
+    await commentReaction(
+      req,
+      res,
+      level,
+      author,
+      commentID,
+      treeID,
+      decoded.id,
+      reactionType,
+      action
+    );
+
+    const comment = await Comment.findById(treeID);
+
+    if (!comment) {
+      throw new AppError(
+        "Comment doesn't exist",
+        404,
+        "COMMENT_NOT_FOUND"
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        comment,
+      },
+    });
   });
 
+
+// ====================
+// Basic CRUD
+// ====================
+
+exports.getAllComments = async (req, res) => {
+  return handlerController.getAll(req, res, Comment);
+};
+
+exports.createComment = asyncHandler(async (req, res) => {
+  const user = await getLoggedUser(req);
+
+  if (!req.body.tree) {
+    throw new AppError(
+      "Comment tree data is required",
+      400,
+      "COMMENT_TREE_REQUIRED"
+    );
+  }
+
+  req.body.tree.author = user._id;
+
+  const comment = await Comment.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: comment,
+    },
+  });
+});
+
+exports.updateComment = async (req, res) => {
+  return handlerController.update(req, res, Comment);
+};
+
+exports.getCommentById = asyncHandler(async (req, res) => {
   const comment = await Comment.findById(req.params.id);
-  res.status(200).json({ status: "success", data: comment });
+
+  if (!comment) {
+    throw new AppError(
+      "Comment doesn't exist",
+      404,
+      "COMMENT_NOT_FOUND"
+    );
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: comment,
+    },
+  });
+});
+
+exports.deleteComment = async (req, res) => {
+  return handlerController.delete(req, res, Comment);
 };
 
-exports.updateLevelThreeRespond = async (req, res) => {
-  try {
-    const decoded = await decodingToken(req);
 
-    // 3) Checking if USER still exists
-    const user = await User.findById(decoded.id);
+// ====================
+// Replies
+// ====================
 
-    req.body.author = user._id;
+exports.updateLevelTwoRespond = asyncHandler(async (req, res) => {
+  const user = await getLoggedUser(req);
 
-    await Comment.findOneAndUpdate(
-      {
-        "tree.replies": {
-          $elemMatch: {
-            author: req.body.repliesTo,
-            _id: req.body.repliesToComment,
-          },
-        },
-        _id: req.params.id,
+  req.body.author = user._id;
+
+  const comment = await Comment.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: {
+        "tree.replies": req.body,
       },
-      {
-        $push: {
-          "tree.replies.$.subReplies": req.body,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!comment) {
+    throw new AppError(
+      "Comment doesn't exist",
+      404,
+      "COMMENT_NOT_FOUND"
+    );
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: comment,
+  });
+});
+
+exports.updateLevelThreeRespond = asyncHandler(async (req, res) => {
+  const user = await getLoggedUser(req);
+
+  req.body.author = user._id;
+
+  const comment = await Comment.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      "tree.replies": {
+        $elemMatch: {
+          author: req.body.repliesTo,
+          _id: req.body.repliesToComment,
         },
-      }
+      },
+    },
+    {
+      $push: {
+        "tree.replies.$.subReplies": req.body,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!comment) {
+    throw new AppError(
+      "Comment or reply doesn't exist",
+      404,
+      "COMMENT_REPLY_NOT_FOUND"
     );
-
-    const commentFresh = await Comment.findOne({ _id: req.params.id });
-
-    res.status(200).json({ status: "success", data: commentFresh });
-  } catch (err) {
-    res.status(404).json({ status: "fail", err });
-    console.log(err);
   }
-};
 
-// ==============
+  res.status(200).json({
+    status: "success",
+    data: comment,
+  });
+});
 
+
+// ====================
 // Reactions
-exports.reactToComment = async (req, res) => {
-  try {
-    const treeID = req.params.treeID;
-    const decoded = await decodingToken(req);
-    const loggedUser = await User.findById(decoded.id);
+// ====================
 
-    const { level, author, reactionType, commentID } = req.body;
-    let comment, reactionsCount, likesCount, disLikesCount;
-    if (!level || !author || !reactionType)
-      throw `Body misses data. Check if contains: "level", "author", "reactionType`;
+exports.reactToComment = handleCommentReaction("add");
 
-    switch (reactionType) {
-      case "like":
-      case "disLike":
-        break;
-      default:
-        throw `"reactionType" can be either 'like' or 'disLike'. Neither: ${reactionType}`;
-    }
-
-    commentReaction(
-      req,
-      res,
-      level,
-      author,
-      commentID,
-      treeID,
-      decoded.id,
-      reactionType,
-      "add"
-    );
-
-    comment = await Comment.findById(treeID);
-    if (!comment) throw `Comment does not exist`;
-    res.status(200).json({
-      status: "success",
-      data: { comment, reactionsCount, likesCount, disLikesCount },
-    });
-  } catch (err) {
-    res.status(404).json({ status: "fail", err });
-  }
-};
-
-exports.removeReaction = async (req, res) => {
-  try {
-    const treeID = req.params.treeID;
-    const decoded = await decodingToken(req);
-    const loggedUser = await User.findById(decoded.id);
-
-    const { level, author, reactionType, commentID } = req.body;
-    let comment, reactionsCount, likesCount, disLikesCount;
-    if (!level || !author || !reactionType)
-      throw `Body misses data. Check if contains: "level", "author", "reactionType`;
-
-    switch (reactionType) {
-      case "like":
-      case "disLike":
-        break;
-      default:
-        throw `"reactionType" can be either 'like' or 'disLike'. Neither: ${reactionType}`;
-    }
-    commentReaction(
-      req,
-      res,
-      level,
-      author,
-      commentID,
-      treeID,
-      decoded.id,
-      reactionType,
-      "remove"
-    );
-
-    comment = await Comment.findById(treeID);
-    if (!comment) throw `Comment does not exist`;
-    res.status(200).json({
-      status: "success",
-      data: { comment, reactionsCount, likesCount, disLikesCount },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err,
-    });
-  }
-};
-
-
+exports.removeReaction = handleCommentReaction("remove");
