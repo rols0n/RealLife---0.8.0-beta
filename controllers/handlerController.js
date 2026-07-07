981c0988488910bbb279cd2327f9a2b7624bf5bc
Patryk Rolka <rolson.priv@gmail.com>
2026-07-07 20:41:58 +0200

    refactor: simplify generic CRUD handlers

--- controllers/handlerController.js
+++ controllers/handlerController.js
@@ -1,94 +1,83 @@
-exports.getAll = async (req, res, Model) => {
-  try {
-    const model = await Model.find();
+const AppError = require("../middlewares/utils/AppError");
 
-    res.status(200).json({
-      status: "success",
-      modelCount: model.length,
-      data: {
-        data: model,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err: {
-        err,
-      },
-    });
-  }
+exports.getAll = async (req, res, Model) => {
+  const models = await Model.find();
+
+  res.status(200).json({
+    status: "success",
+    modelCount: models.length,
+    data: {
+      data: models,
+    },
+  });
 };
 
 exports.create = async (req, res, Model) => {
-  try {
-    const model = await Model.create(req.body);
+  const model = await Model.create(req.body);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        data: model,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err,
-    });
-  }
+  res.status(201).json({
+    status: "success",
+    data: {
+      data: model,
+    },
+  });
 };
 
 exports.update = async (req, res, Model) => {
-  try {
-    await Model.findByIdAndUpdate(req.params.id, req.body);
-    const updatedModel = await Model.findById(req.params.id);
+  const updatedModel = await Model.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        data: updatedModel,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err: {
-        err,
-      },
-    });
+  if (!updatedModel) {
+    throw new AppError(
+      "Document doesn't exist",
+      404,
+      "DOCUMENT_NOT_FOUND"
+    );
   }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: updatedModel,
+    },
+  });
 };
 
 exports.getById = async (req, res, Model) => {
-  try {
-    const model = await Model.findById(req.params.id);
+  const model = await Model.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        data: model,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err: {
-        err,
-      },
-    });
+  if (!model) {
+    throw new AppError(
+      "Document doesn't exist",
+      404,
+      "DOCUMENT_NOT_FOUND"
+    );
   }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: model,
+    },
+  });
 };
 
 exports.delete = async (req, res, Model) => {
-  try {
-    await Model.findByIdAndRemove(req.params.id);
+  const model = await Model.findByIdAndDelete(req.params.id);
 
-    res.status(200).json({ status: "success", data: {} });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      err: {
-        err,
-      },
-    });
+  if (!model) {
+    throw new AppError(
+      "Document doesn't exist",
+      404,
+      "DOCUMENT_NOT_FOUND"
+    );
   }
+
+  res.status(204).send();
 };
