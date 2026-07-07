07837e8a732b88c0cbcab2ddeda711fff329f82b
Patryk Rolka <rolson.priv@gmail.com>
2026-07-07 21:51:31 +0200

    refactor(page-controller): simplify page rendering and error handling

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,267 +1,45 @@
-const Groups = require("../models/groupModel");
+const Group = require("../models/groupModel");
 const User = require("../models/userModel");
 
 const decodingToken = require("../utils/decodingToken");
 const generatePage = require("../utils/generatePage");
 
-exports.getMainPage = async (req, res, next) => {
-  generatePage(req, res, "mainPage", "mainPage");
-};
+const { asyncHandler } = require("../middlewares/utils/asyncHandler");
+const AppError = require("../middlewares/utils/AppError");
 
-exports.userPage = async (req, res, next) => {
-  try {
-    if (req.params.id === "undefined" || undefined)
-      throw "SOmething went wrong";
-    generatePage(req, res, "userPage", "userPage");
-  } catch (err) {
-    const error = "500 internal server error";
-    const description =
-      "Something went wrong, wait a moment and refresh the page.";
-    console.log(err);
-    return res.status(404).render("error.pug", { error, description });
-  }
-};
-
-exports.friends = async (req, res) => {
-  generatePage(req, res, "userPage", "subPages/friends.pug");
-};
-exports.friends_birthdays = async (req, res) => [
-  generatePage(
-    req,
-    res,
-    "userPage",
-    "subPages/userPage/friends/birthdays.pug"
-  ),
-];
-
-exports.informations = async (req, res) => {
-  generatePage(req, res, "userPage", "subPages/informations");
-};
-
-exports.advancedSettings = async (req, res) => {
-  generatePage(
-    req,
-    res,
-    "userPage",
-    "subPages/userPage/informations/advanced_settings.pug"
-  );
-};
-
-exports.manage_friends = async (req, res) => {
-  generatePage(
-    req,
-    res,
-    "userPage",
-    "subPages/userPage/informations/manage_friends.pug"
-  );
-};
-
-exports.delete_friend = async (req, res) => {
-  generatePage(
-    req,
-    res,
-    "userPage",
-    "subPages/userPage/informations/friends/delete_friend.pug"
-  );
-};
-
-exports.blockList = async (req, res) => {
-  generatePage(
-    req,
-    res,
-    "userPage",
-    "subPages/userPage/informations/blockList/blockList.pug"
-  );
-};
-
-exports.bL_manage_list = async (req, res) => {
-  generatePage(
-    req,
-    res,
-    "userPage",
-    "subPages/userPage/informations/blockList/manage_list.pug"
-  );
-};
-
-exports.movies = async (req, res) => {
-  generatePage(req, res, "userPage", "subPages/movies");
-};
-
-exports.photos = async (req, res) => {
-  generatePage(req, res, "userPage", "subPages/photos");
-};
-
-exports.friendsPage = async (req, res, next) => {
-  const decoded = await decodingToken(req);
-  const user = await User.findById(decoded.id)
-    .populate({ path: "receivedRequests" })
-    .populate({ path: "sentRequests" });
-  res
-    .status(200)
-    .render("friends", { loggedUser: user, user, preview: false });
-};
-
-exports.friendsWithPreview = async (req, res, next) => {
-  generatePage(req, res, "userPage", "friendsCopy");
-};
-
-exports.friendsPage_sent = async (req, res) => {
-  const decoded = await decodingToken(req);
-  const user = await User.findById(decoded.id)
-    .populate({ path: "receivedRequests" })
-    .populate({ path: "sentRequests" });
-  res.status(200).render("managers/friends/sent", {
-    loggedUser: user,
-    user,
-    preview: false,
+const renderGeneratedPage = (page, subPage) =>
+  asyncHandler(async (req, res) => {
+    await generatePage(req, res, page, subPage);
   });
-};
-exports.friendsPage_sent_preview = async (req, res) => {
-  try {
-    generatePage(req, res, "userPage", "managers/friends/sent");
-  } catch (err) {
-    const error = "500 internal server error";
-    const description =
-      "Something went wrong, wait a moment and refresh the page.";
-    console.log(err);
-    return res.status(404).render("error.pug", { error, description });
+
+const getLoggedUser = async (req) => {
+  const decoded = await decodingToken(req);
+  const user = await User.findById(decoded.id);
+
+  if (!user) {
+    throw new AppError("User doesn't exist", 404, "USER_NOT_FOUND");
   }
+
+  return user;
 };
 
-exports.friendsPage_received = async (req, res) => {
+const getFriendsManagerUser = async (req) => {
   const decoded = await decodingToken(req);
+
   const user = await User.findById(decoded.id)
     .populate({ path: "receivedRequests" })
     .populate({ path: "sentRequests" });
-  res.status(200).render("managers/friends/received", {
-    loggedUser: user,
-    user,
-    preview: false,
-  });
-};
-exports.friendsPage_received_preview = async (req, res) => {
-  generatePage(req, res, "userPage", "managers/friends/received");
+
+  if (!user) {
+    throw new AppError("User doesn't exist", 404, "USER_NOT_FOUND");
+  }
+
+  return user;
 };
 
-exports.friendsPage_pYmK = async (req, res) => {
+const getGroupsManagerUser = async (req) => {
   const decoded = await decodingToken(req);
-  const user = await User.findById(decoded.id)
-    .populate({ path: "receivedRequests" })
-    .populate({ path: "sentRequests" });
-  res.status(200).render("managers/friends/peopleYouMayKnow", {
-    loggedUser: user,
-    user,
-    preview: false,
-  });
-};
-exports.friendsPage_pYmK_preview = async (req, res, next) => {
-  generatePage(req, res, "userPage", "managers/friends/peopleYouMayKnow");
-};
 
-exports.loginPage = async (req, res, next) => {
-  res.status(200).render("loginPage");
-};
-
-exports.signupPage = async (req, res, next) => {
-  res.status(200).render("singupPage");
-};
-
-// GROUPS
-
-exports.createGroup = async (req, res) => {
-  try {
-    const decoded = await decodingToken(req);
-    const loggedUser = await User.findById(decoded.id);
-    res.status(200).render("creators/createGroup.pug", { loggedUser });
-  } catch (err) {
-    const error = "500 internal server error";
-    const description =
-      "Something went wrong, wait a moment and refresh the page.";
-    console.log(err);
-    return res.status(404).render("error.pug", { error, description });
-  }
-};
-
-exports.groupPage = async (req, res) => {
-  generatePage(req, res, "groupPage", "subPages/groups/discussion");
-
-};
-
-exports.groupPageRequestsMangaer = async (req, res) => {
-  generatePage(
-    req,
-    res,
-    "groupPage",
-    "subPages/groups/subPages/requests/manageRequests.pug"
-  );
-};
-
-exports.aboutSubPage = async (req, res) => {
-  try {
-    generatePage(req, res, "groupPage", "subPages/groups/about");
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.discussionSubPage = async (req, res) => {
-  try {
-    generatePage(req, res, "groupPage", "subPages/groups/discussion");
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.membersSubPage = async (req, res) => {
-  try {
-    generatePage(req, res, "groupPage", "subPages/groups/members");
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-// SUB PAGES of the SUB PAGES
-exports.administration = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/administration"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.commonMembers = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/commonMembers"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.newToGroup = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/newToGroup"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.groupsManager = async (req, res) => {
-  const decoded = await decodingToken(req);
   const user = await User.findById(decoded.id)
     .populate({ path: "receivedRequests" })
     .populate({ path: "sentRequests" })
@@ -296,190 +74,268 @@ exports.groupsManager = async (req, res) => {
       },
     });
 
+  if (!user) {
+    throw new AppError("User doesn't exist", 404, "USER_NOT_FOUND");
+  }
+
+  return user;
+};
+
+const renderLoggedUserPage = (view) =>
+  asyncHandler(async (req, res) => {
+    const loggedUser = await getLoggedUser(req);
+
+    res.status(200).render(view, { loggedUser });
+  });
+
+const renderFriendsManagerPage = (view) =>
+  asyncHandler(async (req, res) => {
+    const user = await getFriendsManagerUser(req);
+
+    res.status(200).render(view, {
+      loggedUser: user,
+      user,
+      preview: false,
+    });
+  });
+
+// GENERAL PAGES
+
+exports.getMainPage = renderGeneratedPage("mainPage", "mainPage");
+
+exports.userPage = asyncHandler(async (req, res) => {
+  if (!req.params.id || req.params.id === "undefined") {
+    throw new AppError("Invalid user id", 400, "INVALID_USER_ID");
+  }
+
+  await generatePage(req, res, "userPage", "userPage");
+});
+
+exports.friends = renderGeneratedPage(
+  "userPage",
+  "subPages/friends.pug"
+);
+
+exports.friends_birthdays = renderGeneratedPage(
+  "userPage",
+  "subPages/userPage/friends/birthdays.pug"
+);
+
+exports.informations = renderGeneratedPage(
+  "userPage",
+  "subPages/informations"
+);
+
+exports.advancedSettings = renderGeneratedPage(
+  "userPage",
+  "subPages/userPage/informations/advanced_settings.pug"
+);
+
+exports.manage_friends = renderGeneratedPage(
+  "userPage",
+  "subPages/userPage/informations/manage_friends.pug"
+);
+
+exports.delete_friend = renderGeneratedPage(
+  "userPage",
+  "subPages/userPage/informations/friends/delete_friend.pug"
+);
+
+exports.blockList = renderGeneratedPage(
+  "userPage",
+  "subPages/userPage/informations/blockList/blockList.pug"
+);
+
+exports.bL_manage_list = renderGeneratedPage(
+  "userPage",
+  "subPages/userPage/informations/blockList/manage_list.pug"
+);
+
+exports.movies = renderGeneratedPage(
+  "userPage",
+  "subPages/movies"
+);
+
+exports.photos = renderGeneratedPage(
+  "userPage",
+  "subPages/photos"
+);
+
+// FRIENDS
+
+exports.friendsPage = renderFriendsManagerPage("friends");
+
+exports.friendsWithPreview = renderGeneratedPage(
+  "userPage",
+  "friendsCopy"
+);
+
+exports.friendsPage_sent = renderFriendsManagerPage(
+  "managers/friends/sent"
+);
+
+exports.friendsPage_sent_preview = renderGeneratedPage(
+  "userPage",
+  "managers/friends/sent"
+);
+
+exports.friendsPage_received = renderFriendsManagerPage(
+  "managers/friends/received"
+);
+
+exports.friendsPage_received_preview = renderGeneratedPage(
+  "userPage",
+  "managers/friends/received"
+);
+
+exports.friendsPage_pYmK = renderFriendsManagerPage(
+  "managers/friends/peopleYouMayKnow"
+);
+
+exports.friendsPage_pYmK_preview = renderGeneratedPage(
+  "userPage",
+  "managers/friends/peopleYouMayKnow"
+);
+
+// AUTH PAGES
+
+exports.loginPage = (req, res) => {
+  res.status(200).render("loginPage");
+};
+
+exports.signupPage = (req, res) => {
+  res.status(200).render("singupPage");
+};
+
+// GROUPS
+
+exports.createGroup = renderLoggedUserPage(
+  "creators/createGroup.pug"
+);
+
+exports.groupPage = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/discussion"
+);
+
+exports.groupPageRequestsMangaer = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/requests/manageRequests.pug"
+);
+
+exports.aboutSubPage = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/about"
+);
+
+exports.discussionSubPage = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/discussion"
+);
+
+exports.membersSubPage = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/members"
+);
+
+// GROUP SUBPAGES
+
+exports.administration = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/administration"
+);
+
+exports.commonMembers = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/commonMembers"
+);
+
+exports.newToGroup = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/newToGroup"
+);
+
+exports.groupsManager = asyncHandler(async (req, res) => {
+  const user = await getGroupsManagerUser(req);
+
   res.status(200).render("groupsManager", {
     loggedUser: user,
     user,
     preview: false,
     previewingGroup: "",
   });
-};
+});
 
-exports.groupsManagerWithPreview = async (req, res) => {
-  try {
-    const decoded = await decodingToken(req);
-    const user = await User.findById(decoded.id)
-      .populate({ path: "receivedRequests" })
-      .populate({ path: "sentRequests" })
-      .populate({
-        path: "groups",
-        populate: {
-          path: "currentlyIn",
-          populate: {
-            path: "_id",
-            ref: "Groups",
-          },
-        },
-      })
-      .populate({
-        path: "groups",
-        populate: {
-          path: "requests.received",
-          populate: {
-            path: "group",
-            ref: "Groups",
-          },
-        },
-      })
-      .populate({
-        path: "groups",
-        populate: {
-          path: "requests.sent",
-          populate: {
-            path: "group",
-            ref: "Groups",
-          },
-        },
-      });
+exports.groupsManagerWithPreview = asyncHandler(async (req, res) => {
+  const [user, previewingGroup] = await Promise.all([
+    getGroupsManagerUser(req),
+    Group.findById(req.params.id),
+  ]);
 
-    const previewingGroup = await Groups.findById(req.params.id);
-
-    res.status(200).render("groupsManager", {
-      loggedUser: user,
-      user,
-      preview: false,
-      previewingGroup,
-    });
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.groupSettings = async (req, res) => {
-  try {
-    generatePage(req, res, "groupPage", "subPages/groups/settings");
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
-
-exports.manage_administration = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/manage_administration"
+  if (!previewingGroup) {
+    throw new AppError(
+      "Group doesn't exist",
+      404,
+      "GROUP_NOT_FOUND"
     );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
   }
-};
 
-exports.adminsManager = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/subPages/admins"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
+  res.status(200).render("groupsManager", {
+    loggedUser: user,
+    user,
+    preview: false,
+    previewingGroup,
+  });
+});
 
-exports.modsManager = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/subPages/mods"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
+// GROUP SETTINGS
 
-// --
+exports.groupSettings = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/settings"
+);
 
-exports.manage_members = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/manage_members"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
+exports.manage_administration = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/manage_administration"
+);
 
-exports.remove_member = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/subPages/remove_member"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
+exports.adminsManager = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/subPages/admins"
+);
 
-exports.advanced_settings = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/advanced_settings"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
+exports.modsManager = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/subPages/mods"
+);
 
-exports.rules = async (req, res) => {
-  try {
-    generatePage(
-      req,
-      res,
-      "groupPage",
-      "subPages/groups/subPages/settings/rules"
-    );
-  } catch (error) {
-    res.status(404).json({ status: "fail", error });
-  }
-};
+exports.manage_members = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/manage_members"
+);
 
-exports.notifications_post = async (req, res) => {
-  try {
-    const decoded = await decodingToken(req);
-    const loggedUser = await User.findById(decoded.id);
-    res.status(200).render("notifications/post/user's", { loggedUser });
-  } catch (err) {
-    const error = "500 internal server error";
-    const description =
-      "Something went wrong, wait a moment and refresh the page.";
-    console.log(err);
-    return res.status(404).render("error.pug", { error, description });
-  }
-};
+exports.remove_member = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/subPages/remove_member"
+);
+
+exports.advanced_settings = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/advanced_settings"
+);
+
+exports.rules = renderGeneratedPage(
+  "groupPage",
+  "subPages/groups/subPages/settings/rules"
+);
+
+// NOTIFICATIONS
+
+exports.notifications_post = renderLoggedUserPage(
+  "notifications/post/user's"
+);
 
 // CHAT
-exports.chats = async (req, res) => {
-  try {
-    const decoded = await decodingToken(req);
-    const loggedUser = await User.findById(decoded.id);
-    res.status(200).render("chats/chats.pug", { loggedUser });
-  } catch (err) {
-    const error = "500 internal server error";
-    const description =
-      "Something went wrong, wait a moment and refresh the page.";
-    console.log(err);
-    return res.status(404).render("error.pug", { error, description });
-  }
-};
+
+exports.chats = renderLoggedUserPage("chats/chats.pug");
